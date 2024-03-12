016d942a4647eb2174b688c8f54546fc37fc96a8
Renze Nicolai <renze@rnplus.nl>
2024-03-12 19:46:45 +0100

    Add deployment CI, remove analytics

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+git submodule update --init --recursive
+#npm install
+#cd themes/docsy
+#npm install
+#cd ../..
+export HUGO_ENV="production"
+hugo --gc --minify
